1df3c5ea78728c1951044c353b7b8a5199472e21
Ali <utfcystgvytf@gmail.com>
2026-08-23 11:34:28 +0300

    Update counter items and format review strings for consistency

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,9 +29,9 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 1, suffix: "+", label: "Years of Experience" },
+  { value: 3, suffix: "+", label: "Satisfied Clients" },
+  { value: 4, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
@@ -147,7 +147,8 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    review:
+      "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
     imgPath: "/images/exp1.png",
     logoPath: "/images/logo1.png",
     title: "Frontend Developer",
@@ -159,7 +160,8 @@ const expCards = [
     ],
   },
   {
-    review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    review:
+      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
     imgPath: "/images/exp2.png",
     logoPath: "/images/logo2.png",
     title: "Full Stack Developer",
@@ -171,7 +173,8 @@ const expCards = [
     ],
   },
   {
-    review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    review:
+      "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
     imgPath: "/images/exp3.png",
     logoPath: "/images/logo3.png",
     title: "React Native Developer",
@@ -248,23 +251,22 @@ const socialImgs = [
   {
     name: "insta",
     imgPath: "/images/insta.png",
-    link: "https://www.instagram.com/3li.__4/"
+    link: "https://www.instagram.com/3li.__4/",
   },
   {
     name: "fb",
     imgPath: "/images/fb.png",
-    link:"https://www.facebook.com/profile.php?id=100016831995557&locale=ar_ARx"
+    link: "https://www.facebook.com/profile.php?id=100016831995557&locale=ar_ARx",
   },
   {
     name: "x",
     imgPath: "/images/x.png",
-    link:"https://x.com/AliElnoby911"
-    
+    link: "https://x.com/AliElnoby911",
   },
   {
     name: "linkedin",
     imgPath: "/images/linkedin.png",
-    link: "https://www.linkedin.com/in/ali-alnoby-79b5673a1/"
+    link: "https://www.linkedin.com/in/ali-alnoby-79b5673a1/",
   },
 ];
 // const socialLinks[
